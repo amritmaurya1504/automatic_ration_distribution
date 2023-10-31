a6db3f163fc3290f6bdec6bc657681c165204235
Amrit Raj <amrit.raj1504@gmail.com>
2023-10-31 22:56:09 +0530

    cached

--- server/app.py
+++ server/app.py
@@ -1,18 +1,19 @@
 from flask import Flask, jsonify
-
 import server.config.app_config
 from server.controllers.user_controller import user_blueprint
 from server.config.db import db
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Initialize JWT
 app.config['JWT_SECRET_KEY'] = server.config.app_config.Config.JWT_SECRET_KEY
 jwt = JWTManager(app)
 
 
-@app.route('/')
+@app.route('/') 
 def hello_world():
     return jsonify({'message': 'Automatic Ration Distribution System Server!'})
 
